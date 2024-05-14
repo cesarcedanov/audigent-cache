2e309239a884b202a151b91ec0d559be1103d69c
CesarCedanoV <cesarcedanov@gmail.com>
2024-05-14 01:08:56 -0400

    Testing and Debugging to check logic

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello Audigent")
+
+	ttlCache := NewCache(2 * time.Second)
+
+	ttlCache.Set([]byte("entry-1"), []byte("value #1"), 5*time.Second)
+	ttlCache.Set([]byte("entry-2"), []byte("value #2"), 10*time.Second)
+	ttlCache.Set([]byte("entry-3"), []byte("value #3"), 1*time.Second)
+	ttlCache.Set([]byte("entry-4"), []byte("value #4"), 2*time.Minute)
+
+	val, ttl := ttlCache.Get([]byte("entry-3"))
+	fmt.Printf("Value %+v  - TTL: %+v\n\n", val, ttl)
+	time.Sleep(3 * time.Second)
+	val, ttl = ttlCache.Get([]byte("entry-3"))
+	fmt.Printf("Value %+v  - TTL: %+v\n\n", val, ttl)
+	val, ttl = ttlCache.Get([]byte("entry-1"))
+	fmt.Printf("Value %+v  - TTL: %+v\n\n", val, ttl)
+	time.Sleep(3 * time.Second)
+	val, ttl = ttlCache.Get([]byte("entry-1"))
+	fmt.Printf("Value %+v  - TTL: %+v\n\n", val, ttl)
+	val, ttl = ttlCache.Get([]byte("entry-4"))
+	fmt.Printf("Value %+v  - TTL: %+v\n\n", val, ttl)
 }
